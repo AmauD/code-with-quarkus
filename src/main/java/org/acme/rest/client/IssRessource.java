b16ade52bf24c66f1ace7b19ff3075581c30e7b8
AmauD <amaury.deltenre@gmail.com>
2022-07-14 13:14:37 +0200

    diagram made with mermaid

--- src/main/java/org/acme/rest/client/IssRessource.java
+++ src/main/java/org/acme/rest/client/IssRessource.java
@@ -1,28 +1,21 @@
 package org.acme.rest.client;
 
 import io.vertx.core.json.JsonObject;
-import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.FormatStyle;
 import java.util.Date;
 import java.util.Objects;
-import java.util.TimeZone;
 
 @Path("/iss")
 public class IssRessource {
--- src/test/java/org/acme/rest/client/IssRessourceTest.java
+++ src/test/java/org/acme/rest/client/IssRessourceTest.java
@@ -4,12 +4,9 @@ import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
-import java.util.Date;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.isA;
-import static org.junit.jupiter.api.Assertions.*;
 
 @QuarkusTest
 class IssRessourceTest {
